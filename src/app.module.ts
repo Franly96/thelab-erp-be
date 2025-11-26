6129713e8b8cbca076a99220c13309c57ada040d
Franly Pena <22371923+Franly96@users.noreply.github.com>
2025-11-26 03:39:57 -0400

    Removes unused default controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 import { CoreModule } from './core/core.module';
 import { InventoryModule } from './inventory/inventory.module';
-import { UsersModule } from './users/users.module';
 import { SalesModule } from './sales/sales.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -23,7 +21,5 @@ import { SalesModule } from './sales/sales.module';
     AuthModule,
     SalesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
